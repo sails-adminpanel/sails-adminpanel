6ba93aa00037d13b93915bd2899fd10ca858c893
Xziy <x@42.pub>
2022-02-21 01:54:03 +0300

    add username in head

--- actions/welcome.js
+++ actions/welcome.js
@@ -14,7 +14,5 @@ module.exports = function(req, res) {
         return res.redirect('/admin/userap/login');
     }
 
-    // if (sails.config.adminpanel.auth) req.locals.user = req.session.UserAP;
-
     return res.viewAdmin('welcome',{ instance: "instance"});
 };
