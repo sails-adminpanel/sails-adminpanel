ffb77bab5a28a30c913d49fc4c061507b5560a7c
Xziy <x@42.pub>
2022-02-21 01:39:35 +0300

    add flash login

--- actions/login.js
+++ actions/login.js
@@ -4,23 +4,25 @@ var passwordHash = require('password-hash');
 var created = false;
 
 module.exports = function(req, res) {
-    console.log(122222)
     if (req.url.indexOf('login') >= 0) {
         if (req.method.toUpperCase() === 'POST') {
             var username = req.param('username');
             var password = req.param('password');
             UserAP.findOne({username: username}).exec((err, user) => {
                 if (err) return res.serverError(err);
-                if (!user) return res.notFound();
-                if (passwordHash.verify(username + password, user.passwordHashed)) {
-                    req.session.UserAP = user;
-                    res.redirect('/admin/');
-                } else {
-                    res.forbidden('Wrong username/password');
+                if (!user) {
+                    req.flash('adminError','Wrong username or password');
+                    return res.viewAdmin('login');
+                } else {   
+                    if (passwordHash.verify(username + password, user.passwordHashed)) {
+                        req.session.UserAP = user;
+                        return res.redirect('/admin/');
+                    } else {
+                        req.flash('adminError','Wrong username or password');
+                        return res.viewAdmin('login');
+                    }
                 }
             });
-        } else if (req.method.toUpperCase() === 'GET') {
-            return res.viewAdmin('login');
         }
     } else if (req.url.indexOf('logout') >= 0) {
         req.session.UserAP = undefined;
--- actions/welcome.js
+++ actions/welcome.js
@@ -14,7 +14,7 @@ module.exports = function(req, res) {
         return res.redirect('/admin/userap/login');
     }
 
-    if (sails.config.adminpanel.auth) req.locals.user = req.session.UserAP;
+    // if (sails.config.adminpanel.auth) req.locals.user = req.session.UserAP;
 
     return res.viewAdmin('welcome',{ instance: "instance"});
 };
