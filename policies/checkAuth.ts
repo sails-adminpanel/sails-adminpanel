9a7b5b5d1ad3723c5c7f3b09eb41a2050df49935
Roman Serebriakov <rserebryakov1902@gmail.com>
2022-06-06 21:21:22 +0300

    checkAuth fix

--- policies/checkAuth.ts
+++ policies/checkAuth.ts
@@ -1,8 +1,12 @@
 module.exports = async function (req, res, proceed) {
-    if (!sails.config.adminpanel.auth && !req.session.UserAP) {
-        req.session.UserAP = {
-            id: "auth-false",
-            isAdministrator: true
+    if (!sails.config.adminpanel.auth) {
+        if (req.session.UserAP) {
+            req.session.UserAP.isAdministrator = true;
+        } else {
+            req.session.UserAP = {
+                id: "auth-false",
+                isAdministrator: true
+            }
         }
     }
     return proceed()
