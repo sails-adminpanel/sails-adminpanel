b12f34acd9183e2bdaee4769ed88485c61806eb4
Roman Serebriakov <rserebryakov1902@gmail.com>
2022-06-06 18:08:40 +0300

    profile expires added

--- controllers/login.ts
+++ controllers/login.ts
@@ -29,6 +29,10 @@ export default async function login(req, res) {
         return res.viewAdmin("login");
       } else {
         if (passwordHash.verify(login + password, user.passwordHashed)) {
+          if (Date.now() > Date.parse(user.expires)) {
+            req.flash("adminError", "Profile expired, contact the administrator");
+            return res.viewAdmin("login");
+          }
           req.session.UserAP = user;
           return res.redirect(`${sails.config.adminpanel.routePrefix}/`);
         } else {
