26c97b4ea6e2c4525b35f4bab467c27faea3bce3
Xziy <x@42.pub>
2022-12-21 18:03:12 +0300

    wow, here it worked

--- controllers/form.js
+++ controllers/form.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const accessRightsHelper_1 = require("../helper/accessRightsHelper");
 const formHelper_1 = require("../helper/formHelper");
 async function form(req, res) {
-    let slug = req.param('slug');
+    let slug = req.param("slug");
     //Check slug
     if (!slug) {
         return res.notFound();
@@ -21,8 +21,7 @@ async function form(req, res) {
             req.body[prop] = JSON.parse(req.body[prop]);
         }
         catch (e) {
-            if (typeof req.body[prop] === "string" && req.body[prop].replace(/(\r\n|\n|\r|\s{2,})/gm, "") &&
-                e.message !== "Unexpected end of JSON input" && !/Unexpected (token .|number) in JSON at position \d/.test(e.message)) {
+            if (typeof req.body[prop] === "string" && req.body[prop].replace(/(\r\n|\n|\r|\s{2,})/gm, "") && e.message !== "Unexpected end of JSON input" && !/Unexpected (token .|number) in JSON at position \d/.test(e.message)) {
                 sails.log.error(JSON.stringify(req.body[prop]), e);
             }
         }
@@ -31,17 +30,23 @@ async function form(req, res) {
     if (!form) {
         return res.status(404).send("Adminpanel > Form not found");
     }
-    if (req.method.toUpperCase() === 'POST') {
+    if (req.method.toUpperCase() === "POST") {
         if (!req.body) {
             return res.status(500).send("Data is empty");
         }
-        for (let field of Object.keys(req.body)) {
-            // false is undefined 
-            if (form[field].type === "boolean" && !req.body[field]) {
-                await sails.config.adminpanel.forms.set(slug, field, false);
+        // checkboxes processing
+        let checkboxes = [];
+        for (let key in form) {
+            if (form[key].type === "boolean") {
+                checkboxes.push(key);
             }
-            else {
-                await sails.config.adminpanel.forms.set(slug, field, req.body[field]);
+        }
+        for (let field of Object.keys(req.body)) {
+            await sails.config.adminpanel.forms.set(slug, field, req.body[field]);
+        }
+        for (let field of checkboxes) {
+            if (!req.body[field]) {
+                await sails.config.adminpanel.forms.set(slug, field, false);
             }
         }
     }
@@ -56,4 +61,3 @@ async function form(req, res) {
     res.viewAdmin("form", { formData: form, slug: slug });
 }
 exports.default = form;
-;
