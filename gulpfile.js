a5fcc86cfdb2466d4ec5dae0aea391ff2e779071
markusvannorlen <55348513+markusvannorlen@users.noreply.github.com>
2022-12-25 02:01:50 +0300

    add ckeditor5 config

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,9 @@ const merge = require("merge-stream");
 //const { VueLoaderPlugin  } = require('vue-loader')
 const Fpath = require('path')
 const webpack = require('webpack');
-const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const { styles } = require('@ckeditor/ckeditor5-dev-utils');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+
 
 
 const buildFolder = `./assets/build`;
@@ -155,25 +157,37 @@ const jsProd = () => {
 const ckeditorBuild = () => {
 	return gulp.src(`${srcFolder}/scripts/ckeditor5/app.js`, { sourcemaps: true })
 		.pipe(webpackStream({
-			mode: 'development',
+			mode: 'production',
+			devtool: 'source-map',
+			performance: { hints: false },
 			entry: `${srcFolder}/scripts/ckeditor5/app.js`,
 
 			// https://webpack.js.org/configuration/output/
 			output: {
-				path: Fpath.resolve('./assets/build/js/'),
-				filename: 'ck5.js'
+				path: Fpath.resolve('./assets/build/js/ckeditor5'),
+				filename: 'ck5.js',
+				library: 'ClassicEditor',
+				libraryTarget: 'umd',
+				libraryExport: 'default'
 			},
-		
+			plugins: [
+				new CKEditorWebpackPlugin({
+					// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
+					// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
+					language: 'en',
+					additionalLanguages: 'all'
+				}),
+			],
 			module: {
 				rules: [
 					{
 						test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
-		
-						use: [ 'raw-loader' ]
+
+						use: ['raw-loader']
 					},
 					{
 						test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
-		
+
 						use: [
 							{
 								loader: 'style-loader',
@@ -188,31 +202,31 @@ const ckeditorBuild = () => {
 							{
 								loader: 'postcss-loader',
 								options: {
-									postcssOptions: styles.getPostCssConfig( {
+									postcssOptions: styles.getPostCssConfig({
 										themeImporter: {
-											themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+											themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
 										},
 										minify: true
-									} )
+									})
 								}
 							}
 						]
 					}
 				]
 			},
-		
+
 			// Useful for debugging.
 			devtool: 'source-map',
-		
+
 			// By default webpack logs warnings if the bundle is bigger than 200kb.
 			performance: { hints: false }
 		}))
 		.pipe(gulp.dest(`${path.build.js}/ckeditor5/`))
 }
 
-const build = gulp.series(reset, copy_styles_files, scss, js);
+const build = gulp.series(reset, copy_styles_files, scss, js, ckeditorBuild);
 
-const prod = gulp.series(reset, copy_styles_files, scssProd, jsProd)
+const prod = gulp.series(reset, copy_styles_files, scssProd, jsProd, ckeditorBuild)
 
 gulp.task('default', build);
 gulp.task('prod', prod);
