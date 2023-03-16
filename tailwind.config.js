eb6a5ad0cfa3cccb55f714a79a4c6a760a686fe3
markusvannorlen <55348513+markusvannorlen@users.noreply.github.com>
2023-03-17 00:59:15 +0300

    added all styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,12 @@ module.exports = {
 			center: true,
 		},
 		screens: {
-			xxs: {max: '320px'},
-			xs: {max: '375px'},
-			sm: {max: '475px'},
-			md: {max: '768px'},
-			lg: {max: '1024px'},
 			xl: {max: '1200px'},
+			lg: {max: '1024px'},
+			md: {max: '768px'},
+			sm: {max: '475px'},
+			xs: {max: '375px'},
+			xxs: {max: '320px'},
 		},
 		extend: {
 			colors: {
