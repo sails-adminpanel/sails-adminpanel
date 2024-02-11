7a60e4d5149fca7bf032bd5443bb594c30cd1692
Xziy <xziy88@gmail.com>
2024-02-11 17:09:05 +0300

    change for POST on ajax list json

--- controllers/listJson.js
+++ controllers/listJson.js
@@ -5,41 +5,45 @@ const fieldsHelper_1 = require("../helper/fieldsHelper");
 const accessRightsHelper_1 = require("../helper/accessRightsHelper");
 const NodeTable_1 = require("../lib/datatable/NodeTable");
 async function listJson(req, res) {
-    let entity = adminUtil_1.AdminUtil.findEntityObject(req);
-    if (!entity.model) {
-        return res.notFound();
-    }
-    if (sails.config.adminpanel.auth) {
-        if (!req.session.UserAP) {
-            return res.redirect(`${sails.config.adminpanel.routePrefix}/model/userap/login`);
-        }
-        else if (!accessRightsHelper_1.AccessRightsHelper.havePermission(`read-${entity.name}-model`, req.session.UserAP)) {
-            return res.sendStatus(403);
-        }
-    }
-    let records = [];
-    let fields = fieldsHelper_1.FieldsHelper.getFields(req, entity, 'list');
-    let query;
     try {
-        // adminpanel design do not support list of more than 5000 lines per request
-        // !TODO take off this limit :)
-        query = entity.model.find({}).limit(5000);
-    }
-    catch (e) {
-        sails.log.error(e);
-    }
-    fieldsHelper_1.FieldsHelper.getFieldsToPopulate(fields).forEach(function (val) {
-        query.populate(val);
-    });
-    const nodeTable = new NodeTable_1.NodeTable(req.query, entity.model, fields);
-    nodeTable.output((err, data) => {
-        if (err) {
-            console.log(err);
-            return;
+        let entity = adminUtil_1.AdminUtil.findEntityObject(req);
+        if (!entity.model) {
+            return res.notFound();
         }
-        // Directly send this data as output to Datatable
-        return res.send(data);
-    });
+        if (sails.config.adminpanel.auth) {
+            if (!req.session.UserAP) {
+                return res.redirect(`${sails.config.adminpanel.routePrefix}/model/userap/login`);
+            }
+            else if (!accessRightsHelper_1.AccessRightsHelper.havePermission(`read-${entity.name}-model`, req.session.UserAP)) {
+                return res.sendStatus(403);
+            }
+        }
+        let fields = fieldsHelper_1.FieldsHelper.getFields(req, entity, 'list');
+        let query;
+        try {
+            // adminpanel design do not support list of more than 5000 lines per request
+            // !TODO take off this limit :)
+            query = entity.model.find({}).limit(5000);
+        }
+        catch (e) {
+            sails.log.error(e);
+        }
+        fieldsHelper_1.FieldsHelper.getFieldsToPopulate(fields).forEach(function (val) {
+            query.populate(val);
+        });
+        const nodeTable = new NodeTable_1.NodeTable(req.body, entity.model, fields);
+        nodeTable.output((err, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            // Directly send this data as output to Datatable
+            return res.send(data);
+        });
+    }
+    catch (error) {
+        sails.log.error(error);
+    }
 }
 exports.default = listJson;
 ;
--- controllers/listJson.ts
+++ controllers/listJson.ts
@@ -1,50 +1,54 @@
 import { AdminUtil } from "../lib/adminUtil";
 import { FieldsHelper } from "../helper/fieldsHelper";
 import { ConfigHelper } from "../helper/configHelper";
-import {AccessRightsHelper} from "../helper/accessRightsHelper";
+import { AccessRightsHelper } from "../helper/accessRightsHelper";
 import { NodeTable } from "../lib/datatable/NodeTable";
 
 export default async function listJson(req, res) {
-    let entity = AdminUtil.findEntityObject(req);
-    if (!entity.model) {
-        return res.notFound();
-    }
-
-    if (sails.config.adminpanel.auth) {
-        if (!req.session.UserAP) {
-            return res.redirect(`${sails.config.adminpanel.routePrefix}/model/userap/login`);
-        } else if (!AccessRightsHelper.havePermission(`read-${entity.name}-model`, req.session.UserAP)) {
-            return res.sendStatus(403);
-        }
-    }
-
-    let records: any = [];
-    let fields = FieldsHelper.getFields(req, entity, 'list');
-    let query;
     try {
-        // adminpanel design do not support list of more than 5000 lines per request
-        // !TODO take off this limit :)
-        query = entity.model.find({}).limit(5000);
-    } catch (e) {
-        sails.log.error(e);
-    }
 
-    FieldsHelper.getFieldsToPopulate(fields).forEach(function(val) {
-        query.populate(val);
-    });
-
-
-
-
-    const nodeTable = new NodeTable(req.query, entity.model, fields);
-    
-    nodeTable.output((err, data)=>{
-        if (err) {
-        console.log(err);
-        return;
+        let entity = AdminUtil.findEntityObject(req);
+        if (!entity.model) {
+            return res.notFound();
         }
 
-        // Directly send this data as output to Datatable
-        return res.send(data)
-    })
+        if (sails.config.adminpanel.auth) {
+            if (!req.session.UserAP) {
+                return res.redirect(`${sails.config.adminpanel.routePrefix}/model/userap/login`);
+            } else if (!AccessRightsHelper.havePermission(`read-${entity.name}-model`, req.session.UserAP)) {
+                return res.sendStatus(403);
+            }
+        }
+
+        let fields = FieldsHelper.getFields(req, entity, 'list');
+        let query;
+        try {
+            // adminpanel design do not support list of more than 5000 lines per request
+            // !TODO take off this limit :)
+            query = entity.model.find({}).limit(5000);
+        } catch (e) {
+            sails.log.error(e);
+        }
+
+        FieldsHelper.getFieldsToPopulate(fields).forEach(function (val) {
+            query.populate(val);
+        });
+
+
+
+
+        const nodeTable = new NodeTable(req.body, entity.model, fields);
+
+        nodeTable.output((err, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            // Directly send this data as output to Datatable
+            return res.send(data)
+        })
+    } catch (error) {
+        sails.log.error(error)
+    }
 };
