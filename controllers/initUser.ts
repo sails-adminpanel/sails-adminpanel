2d4ed6826c6b6235cb508bfad12e811a91ad2dbc
Xziy <xziy88@gmail.com>
2024-04-03 23:40:02 +0300

    add init locale

--- controllers/initUser.ts
+++ controllers/initUser.ts
@@ -10,25 +10,34 @@ export default async function initUser(req, res) {
 
   if (req.method.toUpperCase() === "POST") {
     let login = req.param("login");
+    let locale = req.param("locale");
     let password = req.param("password");
     let confirm_password = req.param("confirm_password");
 
-    console.log(login, password, confirm_password,123)
+    console.log(login, password, confirm_password, 123)
     if (password !== confirm_password) {
       req.session.messages.adminError.push("Password mismatch");
       return res.viewAdmin("init_user");
     }
-    
+
     try {
       console.log(`Created admin`)
-      await UserAP.create({login: login, password: password, fullName: "Administrator",
-          isActive: true, isAdministrator: true});
+      await UserAP.create(
+        {
+          login: login,
+          password: password,
+          fullName: "Administrator",
+          isActive: true,
+          ...(locale !== undefined && { locale }),
+          isAdministrator: true
+        }
+      );
     } catch (e) {
-        sails.log.error("Could not create administrator profile", e)
-        req.session.messages.adminError.push("Could not create administrator profile");
-        return res.viewAdmin("init_user");
+      sails.log.error("Could not create administrator profile", e)
+      req.session.messages.adminError.push("Could not create administrator profile");
+      return res.viewAdmin("init_user");
     }
-    
+
     return res.redirect(`${sails.config.adminpanel.routePrefix}/`);
   }
 
