637e067c603d435aafdae214b907dc78ce491eb0
markus <markusweblance@gmail.com>
2024-09-13 00:12:51 +0300

    fix test

--- test/integration/accessRightsHelper.test.ts
+++ test/integration/accessRightsHelper.test.ts
@@ -1,15 +1,15 @@
 import "mocha";
-import {expect} from "chai";
-import {AccessRightsHelper} from "../../helper/accessRightsHelper";
+import { expect } from "chai";
+import { AccessRightsHelper } from "../../helper/accessRightsHelper";
 
-describe('Access rights helper test', function () {
+describe('Access rights helper test', function() {
     it("Access rights methods exists", async function() {
         expect(sails.hooks.adminpanel.registerAccessToken).to.exist;
         expect(sails.hooks.adminpanel.getAllAccessTokens).to.exist;
         expect(sails.hooks.adminpanel.havePermission).to.exist;
     })
 
-    it("Default (CRUD) tokens created for every entity test", function () {
+    it("Default (CRUD) tokens created for every entity test", function() {
         let entities = sails.config.adminpanel.models;
         let forms = sails.config.adminpanel.forms.data;
 
@@ -20,8 +20,8 @@ describe('Access rights helper test', function () {
 
 
         let tokensAmount = AccessRightsHelper.getTokens().length;
-        console.log(2,AccessRightsHelper.getTokens());
+        console.log(2, AccessRightsHelper.getTokens());
 
-        expect(tokensAmount).to.equal(4 * (entitiesAmount + formsAmount) + 2 + 1); // 4 for every entity and form + 2 routes for migrations +1 for navigation
+        expect(tokensAmount).to.equal(4 * (entitiesAmount + formsAmount) + 2 + 1 + 1); // 4 for every entity and form + 2 routes for migrations +1 for navigation +1 for mediamanager
     })
 })
