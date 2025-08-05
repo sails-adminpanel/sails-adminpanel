1d531bb889661460d3826a70995ca9bec4a3a452
markus <markusweblance@gmail.com>
2025-08-05 21:07:03 +0300

    added registerSystemModels

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const widgetHandler_1 = require("./lib/widgets/widgetHandler");
-const { MenuHelper } = require('./helper/menuHelper');
-const { ConfigHelper } = require('./helper/configHelper');
-const { AccessRightsHelper } = require('./helper/accessRightsHelper');
-const { InstallStepper } = require("./lib/installStepper/installStepper");
-const CatalogHandler_1 = require("./lib/catalog/CatalogHandler");
-const MediaManagerHandler_1 = require("./lib/media-manager/MediaManagerHandler");
-module.exports = function () {
-    let libInitialize = require("./lib/initialize");
+// CommonJS entry point для Sails.js
+// Динамически загружает ESM модуль
+
+module.exports = function sailsAdminpanel() {
+	let libInitialize = require("./lib/initialize.mjs");
     return {
-        /**
-         * Creating default settings for hook
-         */
-        defaults: require('./lib/defaults').defaults(),
-        configure: require('./lib/configure').default(),
-        initialize: async function initialize(cb) {
-            await libInitialize.default(sails, cb);
+        // Default configuration for the hook
+        defaults: function() {
+            return {
+                adminpanel: {
+                    routePrefix: '/admin',
+                    auth: false
+                }
+            };
         },
-        addMenuItem: function (link, label, icon, group) {
-            throw `Not implemented adminpanel index file addMenuItem`;
+
+        // Configure hook
+        configure: function() {
+            // Configuration logic here
         },
-        addGroup: function (key, title) {
-            throw `Not implemented adminpanel index file addGroup`;
-        },
-        addModelConfig: ConfigHelper.addModelConfig,
-        registerAccessToken: AccessRightsHelper.registerToken,
-        getAllAccessTokens: AccessRightsHelper.getTokens,
-        havePermission: AccessRightsHelper.havePermission,
-        enoughPermissions: AccessRightsHelper.enoughPermissions,
-        getInstallStepper: () => InstallStepper,
-        getWidgetHandler: () => widgetHandler_1.WidgetHandler,
-        getCatalogHandler: () => CatalogHandler_1.CatalogHandler,
-        getMediaManagerHandler: () => MediaManagerHandler_1.MediaManagerHandler,
+		initialize: async function initialize(cb) {
+			await libInitialize.default(sails, cb);
+		},
     };
 };
